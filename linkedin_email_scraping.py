ae8fd006df4dfbd65ddef5c982d133c783bca1db
Ashad <ashad001sp@gmail.com>
2023-11-12 19:57:23 +0500

    extract emails with names

--- linkedin_email_scraping.py
+++ linkedin_email_scraping.py
@@ -4,31 +4,51 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
+import csv
 import pandas as pd
+import json
 
 opt = webdriver.ChromeOptions()
 opt.add_argument("--disable-popup-blocking")
 
-
 tags = [
-    # Put tags here
     "Affiliate Marketing",
     "Performance Marketing",
-    # Add as many tags as possible.
+    "Digital Marketing",
+    "Online Advertising",
+    "Content Marketing",
+    "Social Media Marketing",
+    "Search Engine Optimization (SEO)",
+    "Pay Per Click (PPC)",
+    "Email Marketing",
+    "Influencer Marketing",
+    "Partnership Marketing",
+    "E-commerce Marketing",
+    "Conversion Optimization",
+    "Lead Generation",
+    "Marketing Strategist",
+    "Marketing Director",
+    "Marketing Manager",
+    "Marketing Consultant",
+    "Marketing Agency",
+    "Marketing Expert"
 ]
 
-EMAIL = ['gmail.com', 'outlook.com', 'yahoo.com', 'hotmail.com']
+EMAIL = ['gmail.com' , 'outlook.com', 'yahoo.com', 'hotmail.com']
+
+country = 'us'
 
 driver = webdriver.Chrome(options=opt)
 
 for TAG in tags:
     for email_domain in EMAIL:
-        email_df = pd.DataFrame(columns=["Country", "Tag", "Email"])
-        
-        TAG = TAG.replace(' ', '+')
-        driver.get(f'https://www.google.com/search?q=%22{TAG}%22++-intitle%3A%22profiles%22+-inurl%3A%22dir%2F+%22+email%3A+%22%40{email_domain}%22+site%3Awww.linkedin.com%2Fin%2F+OR+site%3Awww.linkedin.com%2Fpub%2F&sca_esv=579408689&sxsrf=AM9HkKlcbT_aAr_y74r0pDww5DseY1OkPA%3A1699092525996&ei=LRhGZY6lPNSNxc8PhaSu-Ak&ved=0ahUKEwjO787zjKqCAxXURvEDHQWSC58Q4dUDCBA&uact=5&oq=%22{TAG}%22++-intitle%3A%22profiles%22+-inurl%3A%22dir%2F+%22+email%3A+%22%40{email_domain}%22+site%3Awww.linkedin.com%2Fin%2F+OR+site%3Awww.linkedin.com%2Fpub%2F&gs_lp=Egxnd3Mtd2l6LXNlcnAihQEiQWZmaWxpYXRlIE1hcmtldGluZyIgIC1pbnrpdGxlOiJwcm9maWxlcyIgLWludXJsOiJkaXIvICIgZW1haWw6ICJAZ21haWwuY29tIiBzaXRlOnd3dy5saW5rZWRpbi.3mNvbS9wdWIvSABQAFgAcAB4AJABAJgBAKABAKoBALgBA8gBAPgBAeIDBBgAIEE&sclient=gws-wiz-serp')
+        unique_emails = set()  
+        email_df = pd.DataFrame(columns=["Country", "Tag", "Name", "Email"])
 
-        time.sleep(random.uniform(9, 14))
+        TAG = TAG.replace(' ', '+')
+        driver.get(f'https://www.google.com/search?q=%22{TAG}%22++-intitle%3A%22profiles%22+-inurl%3A%22dir%2F+%22+email%3A+%22%40{email_domain}%22+site%3A{country}.linkedin.com%2Fin%2F+OR+site%3A{country}.linkedin.com%2Fpub%2F&sca_esv=580067936&sxsrf=AM9HkKmtLt0TG_t_ljUJfbHjL7qCuN306g%3A1699350983830&ei=xwlKZYOSMpaHxc8P0KK2iAg&ved=0ahUKEwjDkfXdz7GCAxWWQ_EDHVCRDYEQ4dUDCBA&uact=5&oq=%22Affiliate+Marketing%22++-intitle%3A%22profiles%22+-inurl%3A%22dir%2F+%22+email%3A+%22%40gmail.com%22+site%3A{country}.linkedin.com%2Fin%2F+OR+site%3A{country}.linkedin.com%2Fpub%2F&gs_lp=Egxnd3Mtd2l6LXNlcnAigwEiQWZmaWxpYXRlIE1hcmtldGluZyIgIC1pbnRpdGxlOiJwcm9maWxlcyIgLWludXJsOiJkaXIvICIgZW1haWw6ICJAZ21haWwuY29tIiBzaXRlOnBrLmxpbmtlZGluLmNvbS9pbi8gT1Igc2l0ZTpway5saW5rZWRpbi5jb20vcHViL0gAUABYAHAAeACQAQCYAQCgAQCqAQC4AQPIAQD4AQL4AQHiAwQYACBB&sclient=gws-wiz-serp')
+        
+        time.sleep(random.uniform(20, 30))
         scroll_amount = 30
 
         def scroll_down(scroll_amount):
@@ -49,7 +69,7 @@ for TAG in tags:
             email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
             return re.findall(email_pattern, text)
 
-        for _ in range(random.randint(20, 30)):
+        for _ in range(random.randint(2, 4)):
             try:
                 scroll_down(scroll_amount)
                 click_more_results_button()
@@ -59,20 +79,21 @@ for TAG in tags:
 
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, 'html.parser')
-
-        emails = set()
-
-        for element in soup.find_all(['a', 'p', 'span', 'div']):
-            text = element.get_text()
-            extracted_emails = extract_emails(text)
-            for email in extracted_emails:
-                emails.add(email)
-
+        elements = soup.find_all(class_="MjjYud")
         TAG = TAG.replace('+', ' ')
-        for email in emails:
-            new_row = pd.DataFrame({"Country": ["US"], "Tag": [TAG], "Email": [email]})
-            email_df = pd.concat([email_df, new_row], ignore_index=True)
-        email_df.to_csv(f"./csvs/{TAG}_{email_domain}_email_data.csv", index=False)
+        for element in elements:
+            block = element.get_text()
+            name = block.split(' - ')[0]
+            if len(name) > 25:
+                temp = block.split(' ')[:2]
+                name = " ".join(temp)
+            email = extract_emails(block)
+            if name != "" and email != []:
+                if email[0] not in unique_emails:
+                    unique_emails.add(email[0])
+                    new_row = pd.DataFrame({"Country": [country.upper()], "Tag": [TAG], "Name": [name], "Email": [email[0]]})
+                    email_df = pd.concat([email_df, new_row], ignore_index=True)
+
+        email_df.to_csv(f"./csvs/{country.upper()}_{TAG}_{email_domain}_email_data.csv", index=False)
 
 driver.quit()
-
