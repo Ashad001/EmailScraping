baca1e08517e5e21d778369693916094a8dc1789
Ashad <ashad001sp@gmail.com>
2023-12-21 16:03:22 +0500

    multi-threaded combine of csvs

--- combine_csvs.py
+++ combine_csvs.py
@@ -1,12 +1,22 @@
 import os
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
 
-csv_directory = r'.\csvs'
+def process_csv(file):
+    file_path = os.path.join(csv_directory, file)
+    data = pd.read_csv(file_path)
+    return data
+
+csv_directory = r'E:\FifthSem\RAIN\messagingAutomation\usa_csvs'
 csv_files = [file for file in os.listdir(csv_directory) if file.endswith('.csv')]
 print(csv_files)
+
 combined_data = pd.DataFrame(columns=["Country", "Tag", "Name", "Email"])
-for csv_file in csv_files:
-    file_path = os.path.join(csv_directory, csv_file)
-    data = pd.read_csv(file_path)
-    combined_data = pd.concat([combined_data, data], ignore_index=True)
+
+with ThreadPoolExecutor() as executor:
+    futures = [executor.submit(process_csv, file) for file in csv_files]
+    for future in futures:
+        data = future.result()
+        combined_data = pd.concat([combined_data, data], ignore_index=True)
+
 combined_data.to_csv('combined_email_data.csv', index=False)
