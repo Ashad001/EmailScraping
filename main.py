f350cfb4db83506f1bd14f00c24e3a21abcd51d7
Ashad <ashad001sp@gmail.com>
2024-03-28 14:47:03 +0500

    captcha email fix

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def send_email(receiver_email, subject, message):
     msg["From"] = email_sender
     msg["To"] = receiver_email
     msg.attach(MIMEText(message, "plain"))
+    print(f"Sending email to {receiver_email}")
     
     context = ssl.create_default_context()
     server = smtplib.SMTP_SSL(smtp_server, smtp_port, context=context)
@@ -94,19 +95,19 @@ for tag in tags:
             tag = tag.replace(' ', '+')
             # driver.get(f'https://www.google.com/search?q=%22{tag}%22++-intitle%3A%22profiles%22+-inurl%3A%22dir%2F+%22+email%3A+%22%40{email_domain}%22+site%3A{country}.linkedin.com%2Fin%2F+OR+site%3A{country}.linkedin.com%2Fpub%2F&sca_esv=580067936&sxsrf=AM9HkKmtLt0TG_t_ljUJfbHjL7qCuN306g%3A1699350983830&ei=xwlKZYOSMpaHxc8P0KK2iAg&ved=0ahUKEwjDkfXdz7GCAxWWQ_EDHVCRDYEQ4dUDCBA&uact=5&oq=%22Affiliate+Marketing%22++-intitle%3A%22profiles%22+-inurl%3A%22dir%2F+%22+email%3A+%22%40gmail.com%22+site%3A{country}.linkedin.com%2Fin%2F+OR+site%3A{country}.linkedin.com%2Fpub%2F&gs_lp=Egxnd3Mtd2l6LXNlcnAigwEiQWZmaWxpYXRlIE1hcmtldGluZyIgIC1pbnRpdGxlOiJwcm9maWxlcyIgLWludXJsOiJkaXIvICIgZW1haWw6ICJAZ21haWwuY29tIiBzaXRlOnBrLmxpbmtlZGluLmNvbS9pbi8gT1Igc2l0ZTpway5saW5rZWRpbi5jb20vcHViL0gAUABYAHAAeACQAQCYAQCgAQCqAQC4AQPIAQD4AQL4AQHiAwQYACBB&sclient=gws-wiz-serp')
             driver.get(f'https://www.google.com/search?q=%22{tag}%22+%22{state}%22+-intitle%3A%22profiles%22+-inurl%3A%22dir%2F+%22+email%3A+%22%40{email_domain}%22+site%3Awww.linkedin.com%2Fin%2F+OR+site%3Awww.linkedin.com%2Fpub%2F&sca_esv=586505729&sxsrf=AM9HkKlmM9qSBgg1YHjj51rdhrAohck7Ng%3A1701319445914&ei=FRNoZZC8N5KRkdUP09-d4AU&ved=0ahUKEwjQmuTp9OqCAxWSSKQEHdNvB1wQ4dUDCBA&uact=5&oq=%22Affiliate+Marketing%22+%22United+States%22+-intitle%3A%22profiles%22+-inurl%3A%22dir%2F+%22+email%3A+%22%40gmail.com%22+site%3Awww.linkedin.com%2Fin%2F+OR+site%3Awww.linkedin.com%2Fpub%2F&gs_lp=Egxnd3Mtd2l6LXNlcnAilAEiQWZmaWxpYXRlIE1hcmtldGluZyIgIlVuaXRlZCBTdGF0ZXMiIC1pbnRpdGxlOiJwcm9maWxlcyIgLWludXJsOiJkaXIvICIgZW1haWw6ICJAZ21haWwuY29tIiBzaXRlOnd3dy5saW5rZWRpbi5jb20vaW4vIE9SIHNpdGU6d3d3LmxpbmtlZGluLmNvbS9wdWIvSABQAFgAcAB4AJABAJgBAKABAKoBALgBA8gBAPgBAeIDBBgAIEE&sclient=gws-wiz-serp')
-            time.sleep(random.randint(10, 50))
+            time.sleep(random.randint(10, 25))
             scroll_amount = 30
 
             def scroll_down(scroll_amount):
                 driver.execute_script("window.scrollBy(0, window.innerHeight);")
-                time.sleep(random.randint(10, 25))
+                time.sleep(random.randint(5, 30))
 
             def click_more_results_button():
                 try:
                     more_results_button = driver.find_element(By.XPATH, "//*[@id='botstuff']/div/div[3]/div[4]/a[1]/h3/div")
                     more_results_button.click()
                     print('Button Clicked')
-                    time.sleep(random.randint(10, 50))
+                    time.sleep(random.randint(10, 25))
                 except Exception as e:
                     print("More results button not found")
                     return
@@ -114,15 +115,7 @@ for tag in tags:
             def extract_emails(text):
                 email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
                 return re.findall(email_pattern, text)
-
-            for _ in range(random.randint(25, 35)):
-                try:
-                    scroll_down(scroll_amount)
-                    click_more_results_button()
-                except Exception as e:
-                    print(e)
-                    continue
-
+            
             page_source = driver.page_source
             captcha_present = any(keyword in page_source for keyword in ["CAPTCHA", "Captcha", "captcha", "recaptcha", "reCAPTCHA"])
             if captcha_present:
@@ -132,8 +125,18 @@ for tag in tags:
                 with open('./logs/captchas_file.txt', 'a', encoding='utf-8') as f:
                     time_detected = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
                     f.write(f"{time_detected}, {tag}, {state}, {email_domain}\n")
-                exit(0)
+                continue
 
+            for _ in range(random.randint(25, 35)):
+                try:
+                    scroll_down(scroll_amount)
+                    click_more_results_button()
+                except Exception as e:
+                    print(e)
+                    continue
+
+            
+                
             soup = BeautifulSoup(page_source, 'html.parser')
             elements = soup.find_all(class_="MjjYud")
             tag = tag.replace('+', ' ')
@@ -159,8 +162,8 @@ for tag in tags:
             csv_combiner.combine_csvs()
             csv_combiner.save_combined_csv()
             email_checker.process_and_save_emails()
-    if check_flag:
-        upload()
+    # if check_flag:
+    #     upload()
 driver.quit()
 
 subject = "Email Scraping Completed!"
